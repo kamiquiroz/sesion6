aa504446837acc1d223ee923c937d7cf7f8aaad8
kamiquiroz <kamiquiroz@gmail.com>
2021-11-20 10:53:57 -0500

    elements and components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-//Render recibe un elemento y el contenedor donde
-//se mostrará en mi página HTML
-ReactDOM.render(<h1>Hola a todos!!!</h1>, document.getElementById('root'))
+
+//Componentes: permite separar la interfaz en piezas independientes y está
+//compuesta de elementos
+function getTime() {
+    //Elementos: son los bloques mas pequeños de mi aplicación en React
+    const element = (
+        <div>
+            <h1>Hello everyone!!!</h1>
+            <h2>It is { new Date().toLocaleTimeString() }.</h2>  
+        </div>
+    );
+
+    //Render recibe un elemento y el contenedor donde
+    //se mostrará en mi página HTML
+    ReactDOM.render(element, document.getElementById('root'))
+}
+
+setInterval(getTime,1000);
+
+
